1c0db8d1afdfbe9cd56c956f61da42cb68936d01
Syngc <cynthiagviria@gmail.com>
2017-11-14 23:43:38 -0500

    Ejercicio 5 y 6

---
+++ python/Project Euler/Problem 6/reto.py
@@ -0,0 +1,19 @@
+import math
+
+def sumaCuadrados(x):
+    suma = 0
+    for i in range(1,x + 1):
+        print(i)
+        suma = suma + math.pow(i,2)
+    return suma
+
+def cuadradoSuma(x):
+    suma = 0
+    for i in range(1,x + 1):
+        suma = suma + i
+    suma = math.pow(suma,2)
+    return suma
+
+num = 100
+ans = int(cuadradoSuma(num) - sumaCuadrados(num))
+print(ans)
